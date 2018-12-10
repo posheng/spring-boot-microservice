078ef571f44f03cbac3803e9af2d16073ce5a53c
Samuel Huang <paisheng.h@gmail.com>
2018-12-10 09:54:58 +0800

    Write a Rest Endpoint

--- src/main/java/com/acer/springbootmicroservice/SpringBootMicroserviceApplication.java
+++ src/main/java/com/acer/springbootmicroservice/SpringBootMicroserviceApplication.java
@@ -2,11 +2,19 @@ package com.acer.springbootmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootMicroserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMicroserviceApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello Spring Boot";
     }
 }
